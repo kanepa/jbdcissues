647e5850bfd374ba0fe667cfa2a5d8017713f831
Paul Kane <kanepa1977@gmail.com>
2017-08-25 12:29:28 +0100

    small change

--- DBUtil.java
+++ DBUtil.java
@@ -6,7 +6,10 @@ public class DBUtil {
 	
 	private static final String mySqlUser = "root";
 	private static final String mySqlPWD = "mypass";
-	private static final String mySQLCS = "jdbc:mysql://localhost:3306/?user=root&password=rootpassword\");";
+	private static final String mySQLCS = "jdbc:mysql://localhost:3306/?user=root&password=rootpassword\');";
+// i have tried different code which either gives the console
+//	Access denied for user 'root'@'localhost' (using password: YES)  which is this instant or 
+	// Access denied for user 'root'@'localhost' (using password: no) 
 	
 	public static Connection getConnection(DBType dbType) throws SQLException {
 		switch (dbType) {
