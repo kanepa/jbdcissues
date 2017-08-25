3fe50f3646cef389f17d40ef25fe0c8f7b183b74
Paul Kane <kanepa1977@gmail.com>
2017-08-25 12:41:20 +0100

    further details

--- DBUtil.java
+++ DBUtil.java
@@ -10,6 +10,7 @@ public class DBUtil {
 // i have tried different code which either gives the console
 //	Access denied for user 'root'@'localhost' (using password: YES)  which is this instant or 
 	// Access denied for user 'root'@'localhost' (using password: no) 
+	// The best I can get it is tried three times to hit the connection but ran out of time
 	
 	public static Connection getConnection(DBType dbType) throws SQLException {
 		switch (dbType) {
@@ -24,3 +25,38 @@ public class DBUtil {
 	}
 	
 }
+
+// original file before I start messing with it
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class TestMySQLConnection {
+//
+//	static String username = "root";
+//	static String password = "root";
+//	static String dbUrl = "jdbc:mysql://localhost:3306/world";
+//	
+//	public static void main(String[] args) throws SQLException {
+//		// TODO Auto-generated method stub
+//		
+//		Connection conn = null;
+//		try {
+//			conn = DriverManager.getConnection(dbUrl, username, password);
+//			
+//			System.out.println("Connection Established to MYSQL Database");
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			System.err.println(e.getMessage());
+//		}
+//		finally{
+//			conn.close();
+//		}
+//		
+//		
+//	
+//
+//	}
+//
+//}
+
